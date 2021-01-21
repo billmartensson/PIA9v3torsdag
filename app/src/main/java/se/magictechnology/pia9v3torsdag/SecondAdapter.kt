978dc225f7986a6c93b9add0c0bda8fdf82ad843
Bill Mårtensson <bill@magictechnology.se>
2021-01-21 12:52:04 +0100

    Bild

--- app/src/main/java/se/magictechnology/pia9v3torsdag/SecondAdapter.kt
+++ app/src/main/java/se/magictechnology/pia9v3torsdag/SecondAdapter.kt
@@ -24,7 +24,38 @@ class SecondAdapter() : RecyclerView.Adapter<SecondViewHolder>() {
 
         holder.secondtv.text = fruitnames[position]
 
-        holder.secondimage.setImageResource(R.drawable.frog)
+        /*
+        Bild från namn på bild.
+        1. Hitta context. Tar här från radens vy. Sammanhang.
+        2. Ur den, ta resources
+        3. Få ut identifier för en resource
+        4. Namn på resource
+        5. Vilken typ av resource
+        6. Vilket paket dvs vår att app
+
+          1            2         3           4         5                 6
+        theContext.resources.getIdentifier("frog", "drawable", theContext.packageName)
+
+        Ger tillbaka resource id int samma som R.id.frog
+
+        Använd det i setImageResource
+         */
+
+        var theContext = holder.itemView.context
+
+        if(position == 0)
+        {
+            var frogImage = theContext.resources.getIdentifier("frog", "drawable", theContext.packageName)
+
+            holder.secondimage.setImageResource(frogImage)
+
+        } else {
+            var birdImage = theContext.resources.getIdentifier("bird", "drawable", theContext.packageName)
+
+            holder.secondimage.setImageResource(birdImage)
+
+        }
+
 
         holder.itemView.setOnClickListener {
 
